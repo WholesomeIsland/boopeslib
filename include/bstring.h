d136f773774db918a8dd64e3220e07b80c055f22
WholesomeIsland <65096556+WholesomeIsland@users.noreply.github.com>
2021-10-10 10:58:38 -0400

    added basic string class

---
+++ include/bstring.h
@@ -0,0 +1,12 @@
+#include <string.h>
+
+class(string)
+char* data;
+method_proto(string, int, compare, string* other);
+end_class(string)
+
+method_header(string, int, string_open, string* other)
+string* string_ctor(char* d){
+string* this = new(string);
+this->compare = &string_compare;
+}
---
+++ string/string.c
@@ -0,0 +1,5 @@
+#include "../include/bstring.h"
+#include <string.h>
+method_def(string, int, compare, string* other) {
+	return strcmp(this->data, other->data);	
+}
